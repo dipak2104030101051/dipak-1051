60c3ed4ce89e7667e39621be1f2fab39c70b877f
dipak2104030101051 <104961462+dipak2104030101051@users.noreply.github.com>
2022-07-27 14:42:33 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+  var a = new Date();
+  document.write(a + "<br>"); //date  time fuctiona
+
+  var b = new Date();
+  document.write(b.toDateString() + "<br>");
+  var c = new Date();
+  document.write(c.getDate())
+  var d = new Date();
+  document.write(d.getFullyear());
+  var g = new Date();
+  document.write(g.getMonth() + "<br>");
+  var l = new Date();
+  document.write(l.getDay() + "<br>");
